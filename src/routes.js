c69300b888f24c69af194953105f357bfbd37608
Raphael Gomes Santos <raphael.gomes@ibm.com>
2022-06-09 17:24:22 -0300

    upload files support

--- src/routes.js
+++ src/routes.js
@@ -3,21 +3,23 @@ const { check, validationResult, matchedData } = require('express-validator');
 const email = require('./email');
 const csrf = require('csurf');
 const csrfProtection = csrf({ cookie: true });
+const multer = require('multer');
+const upload = multer({ storage: multer.memoryStorage() });
 
 const router = express.Router();
 router.get('/', (req, res) => {
   res.render('index');
 });
 
-router.get('/contact', csrfProtection, (req, res) => {
+router.get('/contact', (req, res) => {
   res.render('contact', {
-    data: {},
     errors: {},
-    csrfToken: req.csrfToken()
+    data: {},
+    csrfToken: req.csrfToken
   });
 });
 
-router.post('/contact', [
+router.post('/contact', upload.single('photo'), csrfProtection, [
   check('message')
     .isLength({ min: 1 })
     .withMessage('Message is required')
@@ -34,14 +36,19 @@ router.post('/contact', [
     return res.render('contact', {
       data: req.body,
       errors: errors.mapped(),
-      csrfToken: req.csrfToken()
+      csrfToken: req.csrfToken
     });
   }
 
   const data = matchedData(req);
-  console.log('Sanitized:', datça);
+  console.log('Sanitized:', data);
   // Homework: send sanitized data in an email or persist to a db:
   email.send(data.email, 'message-from-node', data.message);
+
+  if (req.file) {
+    console.log('Uploaded: ', req.file);
+    // Homework: Upload file to S3
+  }
 
   req.flash('success', 'Thanks for the message! I‘ll be in touch :)');
   res.redirect('/');
